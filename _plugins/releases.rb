2bc691498a5b3010687691ddaea388bc38a8089a
Michał Kalbarczyk <fazibear@gmail.com>
2025-10-21 23:16:54 +0200

    add current_version.txt

--- _plugins/releases.rb
+++ _plugins/releases.rb
@@ -1,7 +1,7 @@
-Jekyll::Hooks.register :site, :post_read do |site|
-  json = `gh release view --json tagName,assets`
-  releases = JSON.parse(json)
+json = `gh release view --json tagName,assets`
+releases = JSON.parse(json)
 
+Jekyll::Hooks.register :site, :post_read do |site|
   version = releases["tagName"]
   windows = releases["assets"].select{|i| i["url"].match("windows") }.first["url"]
   macos = releases["assets"].select{|i| i["url"].match("macos") }.first["url"]
@@ -11,4 +11,9 @@ Jekyll::Hooks.register :site, :post_read do |site|
   site.data["macos"] = macos
 
   puts site.data
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  file = File.join(site.dest, 'current_version.txt')
+  File.write(file, releases["tagName"])
 end
